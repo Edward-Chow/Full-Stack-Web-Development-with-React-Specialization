4b13b1b7393ef7cbc4f6685a0569edbe2958e519
Gershon-Chow <superm0nk3y@outlook.com>
2019-01-27 09:18:34 +0800

    React Router

--- Front-End-Web-Development-with-React/confusion/src/components/MainComponent.js
+++ Front-End-Web-Development-with-React/confusion/src/components/MainComponent.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
-import DishDetail from './DishdetailComponent'
+import DishDetail from './DishdetailComponent';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
@@ -11,8 +13,7 @@ class Main extends Component {
     super(props);
 
     this.state = {
-      dishes: DISHES,
-      selectedDish: null
+      dishes: DISHES
     };
   }
 
@@ -22,12 +23,21 @@ class Main extends Component {
 }
   
   render() {
+
+    const HomePage = () => {
+      return (
+        <Home />
+      );
+    }
+
     return (
       <div>
         <Header/>
-        <Menu dishes={this.state.dishes} 
-            onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
+          <Redirect to="/home" />
+        </Switch>
         <Footer />
       </div>
     );
--- Front-End-Web-Development-with-React/confusion/src/components/MenuComponent.js
+++ Front-End-Web-Development-with-React/confusion/src/components/MenuComponent.js
@@ -3,7 +3,7 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
 
 function RenderMenuItem({ dish, onClick }) {
     return (
-        <Card onClick={() => onClick(dish.id)}>
+        <Card>
             <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>        
             <CardImgOverlay>
                 <CardTitle>{dish.name}</CardTitle>
