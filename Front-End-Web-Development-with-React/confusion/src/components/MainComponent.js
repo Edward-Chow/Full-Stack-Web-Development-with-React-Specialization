c6f7621ddd789858dbae1ec06cafe0f41da8abce
Gershon-Chow <superm0nk3y@outlook.com>
2019-01-28 20:22:09 +0800

    Assignment 2

--- Front-End-Web-Development-with-React/confusion/src/components/MainComponent.js
+++ Front-End-Web-Development-with-React/confusion/src/components/MainComponent.js
@@ -3,6 +3,7 @@ import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
+import About from './AboutComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
@@ -53,6 +54,7 @@ class Main extends Component {
         <Header/>
         <Switch>
           <Route path="/home" component={HomePage} />
+          <Route path="/aboutus" component={() => <About leaders={this.state.leaders}/>} />
           <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>} />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
