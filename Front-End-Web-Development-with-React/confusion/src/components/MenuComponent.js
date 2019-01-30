b9d1c746325c18b581b1a2901e43ce62536b70dc
Gershon-Chow <superm0nk3y@outlook.com>
2019-01-30 17:32:14 +0800

    Final version

--- Front-End-Web-Development-with-React/confusion/src/components/MenuComponent.js
+++ Front-End-Web-Development-with-React/confusion/src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import {Loading} from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
