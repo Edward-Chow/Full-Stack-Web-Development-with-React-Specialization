bf8cfa51255b91b3a20cd60d7cfa7e4b4251e23a
Gershon-Chow <superm0nk3y@outlook.com>
2019-01-31 13:25:04 +0800

    Components Part 1

--- Multiplatform–Mobile-App-Development-with-React-Native/confusion/App.js
+++ Multiplatform–Mobile-App-Development-with-React-Native/confusion/App.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Main from './components/MainComponent';
+import { SafeAreaView } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <SafeAreaView>
+        <Main />
+      </SafeAreaView>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
