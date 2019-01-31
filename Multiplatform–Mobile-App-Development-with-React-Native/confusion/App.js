90a3c0512d695d21b419e3806daa8da76087f13a
Gershon-Chow <superm0nk3y@outlook.com>
2019-01-31 16:50:19 +0800

    Navigation Part 1

--- Multiplatform–Mobile-App-Development-with-React-Native/confusion/App.js
+++ Multiplatform–Mobile-App-Development-with-React-Native/confusion/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import Main from './components/MainComponent';
-import { SafeAreaView } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <SafeAreaView>
         <Main />
-      </SafeAreaView>
     );
   }
 }
--- Multiplatform–Mobile-App-Development-with-React-Native/confusion/components/MainComponent.js
+++ Multiplatform–Mobile-App-Development-with-React-Native/confusion/components/MainComponent.js
@@ -2,29 +2,35 @@ import React, { Component } from 'react';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
-import { View } from 'react-native';
+import { View, Platform, SafeAreaView } from 'react-native';
+import { createStackNavigator } from 'react-navigation';
 
-class Main extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            dishes: DISHES,
-            selectedDish: null
+const MenuNavigator = createStackNavigator({
+    Menu: { screen: Menu },
+    Dishdetail: { screen: Dishdetail }
+},
+{
+    initialRouteName: 'Menu',
+    navigationOptions: {
+        headerStyle: {
+            backgroundColor: "#512DA8"
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+            color: "#fff"            
         }
     }
-    
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId});
-    }
+}
+);
+class Main extends Component {
 
     render() {
         return (
-            <View>
-                <Menu dishes={this.state.dishes} 
-                    onPress={(dishId) => this.onDishSelect(dishId)}/>
-                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-            </View>
+            <SafeAreaView  style={{flex: 1, backgroundColor: "#512DA8"}}>
+                <View style={{ flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Expo.Constants.statusBarHeight }}>
+                    <MenuNavigator />
+                </View>
+            </SafeAreaView>
         );
     }
 }
