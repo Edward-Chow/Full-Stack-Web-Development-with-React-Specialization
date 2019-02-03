ba9956b3bfc3bde3cc89abf170547824b77435fe
Gershon-Chow <superm0nk3y@outlook.com>
2019-02-03 15:02:35 +0800

    Gestures Part 2

--- Multiplatform–Mobile-App-Development-with-React-Native/confusion/components/DishdetailComponent.js
+++ Multiplatform–Mobile-App-Development-with-React-Native/confusion/components/DishdetailComponent.js
@@ -21,6 +21,8 @@ const mapDispatchToProps = dispatch => ({
 
 function RenderDish(props) {
     const dish = props.dish;
+
+    handleViewRef = ref => this.view = ref;
     
     const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
         if (dx < -200)
@@ -33,6 +35,7 @@ function RenderDish(props) {
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
         },
+        onPanResponderGrant: () => {this.view.rubberBand(1000).then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));},
         onPanResponderEnd: (e, gestureState) => {
             if (recognizeDrag(gestureState))
                 Alert.alert(
@@ -58,6 +61,7 @@ function RenderDish(props) {
     if (dish != null) {
         return (
             <Animatable.View animation="fadeInDown" duration={2000} delay={1000} 
+                ref={this.handleViewRef}
                 {...panResponder.panHandlers}>
                 <Card
                     featuredTitle={dish.name}
