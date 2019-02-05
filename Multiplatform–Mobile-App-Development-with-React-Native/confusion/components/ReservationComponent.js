f0e1e0f0ab15f4af71e528a86df02a339aed2250
Gershon-Chow <superm0nk3y@outlook.com>
2019-02-05 22:32:27 +0800

    Local Notifications

--- Multiplatform–Mobile-App-Development-with-React-Native/confusion/components/ReservationComponent.js
+++ Multiplatform–Mobile-App-Development-with-React-Native/confusion/components/ReservationComponent.js
@@ -3,6 +3,7 @@ import { ScrollView, Text, View, StyleSheet, Picker, Switch, Alert, Button, Moda
 import { Card } from 'react-native-elements';
 import DatePicker from 'react-native-datepicker';
 import * as Animatable from 'react-native-animatable';
+import { Permissions, Notifications } from 'expo';
 
 class Reservation extends Component {
     constructor(props) {
@@ -32,7 +33,10 @@ class Reservation extends Component {
                 },
                 {
                     text: 'OK',
-                    onPress: () => this.resetForm()
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date);
+                        this.resetForm();
+                    }
                 }
             ]
         )
@@ -43,6 +47,33 @@ class Reservation extends Component {
             guests: 1,
             smoking: false,
             date: ''
+        });
+    }
+
+    async obtainNotificationPermission() {
+        let permission = await Permissions.getAsync(Permissions.USER_FACING_NOTIFICATIONS);
+        if (permission.status !== 'granted') {
+            permission = await Permissions.askAsync(Permissions.USER_FACING_NOTIFICATIONS);
+            if (permission.status !== 'granted') {
+                Alert.alert('Permission not granted to show notifications');
+            }
+        }
+        return permission;
+    }
+
+    async presentLocalNotification(date) {
+        await this.obtainNotificationPermission();
+        Notifications.presentLocalNotificationAsync({
+            title: 'Your Reservation',
+            body: 'Reservation for '+ date + ' requested',
+            ios: {
+                sound: true
+            },
+            android: {
+                sound: true,
+                vibrate: true,
+                color: '#512DA8'
+            }
         });
     }
 
