65ae1fab12c357e27662aa4fd819d9f958a09c2f
Gershon-Chow <superm0nk3y@outlook.com>
2019-02-11 17:25:00 +0800

    Mongoose Part 2

--- Server-side-Development-with-NodeJS-Express-and-MongoDB/NodeJS/node-mongoose/index.js
+++ Server-side-Development-with-NodeJS-Express-and-MongoDB/NodeJS/node-mongoose/index.js
@@ -7,25 +7,23 @@ const connect = mongoose.connect(url);
 
 connect.then((db) => {
     console.log('Connected correctly to server');
-    var newDish = Dishes({
+
+    Dishes.create({
         name: 'Uthappizza',
         description: 'test'
+    })
+    .then((dish) => {
+        console.log(dish);
+        return Dishes.find({});
+    })
+    .then((dishes) => {
+        console.log(dishes);
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-    newDish.save()
-        .then((dish) => {
-            console.log(dish);
-
-            return Dishes.find({});
-        })
-        .then((dishes) => {
-            console.log(dishes);
-
-            return Dishes.remove({});
-        })
-        .then(() => {
-            return mongoose.connection.close();
-        })
-        .catch((err) => {
-            console.log(err);
-        });
 });
