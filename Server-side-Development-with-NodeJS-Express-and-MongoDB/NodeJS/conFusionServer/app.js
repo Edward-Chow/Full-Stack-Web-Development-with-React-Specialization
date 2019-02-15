829cd4924f138c4352324d89f35e7219222bd92b
Gershon-Chow <superm0nk3y@outlook.com>
2019-02-15 21:13:02 +0800

    Passport JWT

--- Server-side-Development-with-NodeJS-Express-and-MongoDB/NodeJS/conFusionServer/app.js
+++ Server-side-Development-with-NodeJS-Express-and-MongoDB/NodeJS/conFusionServer/app.js
@@ -7,6 +7,7 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 var passport = require('passport');
 var authenticate = require('./authenticate');
+var config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -18,7 +19,7 @@ const mongoose = require('mongoose');
 
 const Dishes = require('./models/dishes');
 
-const url = "mongodb://localhost:27017/conFusion";
+const url = config.mongoUrl;
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
@@ -36,33 +37,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser('12345-67890-09876-54321'));
 
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
-
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-function auth(req, res, next) {
-
-  if (!req.user) {
-    var err = new Error('You are not authenticated!');
-    err.status = 403;
-    return next(err);
-  } else {
-    next();
-  }
-
-}
-
-app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
