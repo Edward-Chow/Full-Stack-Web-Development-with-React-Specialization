d725131dd2b168070c3efe69d9420a6f651882d0
Gershon-Chow <superm0nk3y@outlook.com>
2019-02-18 18:26:48 +0800

    Passport Facebook

--- Server-side-Development-with-NodeJS-Express-and-MongoDB/NodeJS/conFusionServer/config.js
+++ Server-side-Development-with-NodeJS-Express-and-MongoDB/NodeJS/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '380952162694227',
+        clientSecret: 'ccf5379a702c09e91b5bb80c8a31ed13'
+    }
 }
